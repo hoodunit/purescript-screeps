4d2e0d202628496b42b0cd471be377a59b9d77ee
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2016-08-10 00:08:52 +0300

    Remove unused Map terrain functions

--- src/Screeps/Map.purs
+++ src/Screeps/Map.purs
@@ -15,6 +15,12 @@ type ExitsInfo =
   , "5" :: String
   , "7" :: String }
 
+type RoomRoute = Array ExitToRoom
+
+type ExitToRoom =
+  { exit :: FindType Unit
+  , room :: String }
+
 describeExits :: String -> Maybe ExitsInfo
 describeExits name = toMaybe $ runThisFn1 "attackController" Game.map name
 
@@ -24,11 +30,6 @@ findExit from to = runThisFn2 "findExit" Game.map from to
 
 findExit' :: String -> String -> ReturnCode
 findExit' from to = runThisFn2 "findExit" Game.map from to
-
-type RoomRoute = Array ExitToRoom
-type ExitToRoom =
-  { exit :: FindType Unit
-  , room :: String }
 
 -- TODO: options
 -- TODO: handle returning errors
@@ -40,15 +41,6 @@ findRoute' from to = runThisFn2 "findRoute" Game.map from to
 
 getRoomLinearDistance :: String -> String -> Int
 getRoomLinearDistance name1 name2 = runThisFn2 "getRoomLinearDistance" Game.map name1 name2
-
-terrain_plain :: Terrain
-terrain_plain = Terrain "plain"
-
-terrain_swamp :: Terrain
-terrain_swamp = Terrain "swamp"
-
-terrain_wall :: Terrain
-terrain_wall = Terrain "wall"
 
 getTerrainAt :: Int -> Int -> String -> Terrain
 getTerrainAt x y roomName = runThisFn3 "getTerrainAt" Game.map x y roomName
