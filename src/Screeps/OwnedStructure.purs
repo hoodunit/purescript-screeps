eef98c3d9795a79e727a7e7fe08e578e7f5489af
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2016-08-11 22:22:12 +0300

    Properly return maybe from OwnedStructure owner, my functions

--- src/Screeps/OwnedStructure.purs
+++ src/Screeps/OwnedStructure.purs
@@ -1,11 +1,14 @@
 -- | Corresponds to the Screeps API [OwnedStructure](http://support.screeps.com/hc/en-us/articles/207710979-OwnedStructure)
 module Screeps.OwnedStructure where
 
+import Prelude
+import Data.Maybe (Maybe, fromMaybe)
+
 import Screeps.Types (OwnedStructure)
-import Screeps.FFI (unsafeField)
+import Screeps.FFI (toMaybe, unsafeField)
 
 my :: forall a. OwnedStructure a -> Boolean
-my = unsafeField "my"
+my struc = fromMaybe false $ toMaybe $ unsafeField "my" struc
 
-owner :: forall a. OwnedStructure a -> { username :: String }
-owner = unsafeField "owner"
+owner :: forall a. OwnedStructure a -> Maybe { username :: String }
+owner struc = toMaybe $ unsafeField "owner" struc
