8f5b236eda5d022bd4ad6fe33f2a440c099329b6
Michal Gajda <michal@jewelpaymentech.com>
2017-01-10 05:12:46 +0800

    PathFinder API with caching.

---
+++ src/Screeps/Path.purs
@@ -0,0 +1,137 @@
+module Screeps.Path where
+
+import Control.Monad.Eff
+import Data.Argonaut.Core (Json)
+import Data.Array
+import Data.Generic
+import Data.Show
+import Data.Unit
+
+import Screeps.FFI
+import Screeps.Path
+import Screeps.RoomObject
+import Screeps.RoomPosition.Type
+
+-- * Tile cost
+type TileCost = Int
+
+defaultTerrainCost :: TileCost
+defaultTerrainCost  = 0
+
+unwalkable :: TileCost
+unwalkable  = 255
+
+foreign import data PATH :: !
+
+newtype PathFinderResult = PathFinderResult {
+    path       :: Array RoomPosition
+  , opts       :: Int
+  , cost       :: Int
+  , incomplete :: Boolean
+  }
+
+--derive instance genericPathFinderTarget :: Generic PathFinderTarget
+--derive instance genericPathFinderResult :: Generic PathFinderResult
+--derive instance showPathFinderTarget :: Show PathFinderTarget --where show = gShow
+--derive instance showPathFinderResult :: Show PathFinderResult --where show = gShow
+
+newtype PathFinderTarget = PathFinderTarget {
+    pos   :: RoomPosition
+  , range :: Int
+  }
+
+target     :: RoomPosition
+           -> PathFinderTarget
+target aPos = PathFinderTarget {
+                pos  : aPos
+              , range: 0
+              }
+
+inRange           :: RoomPosition
+                  -> Int
+                  -> PathFinderTarget
+inRange aPos range = PathFinderTarget {
+                       pos  : aPos
+                     , range: range
+                     }
+
+foreign import usePathFinder :: forall              e.
+                                Boolean
+                             -> Eff (path :: PATH | e) Unit
+
+foreign import data CostMatrix :: *
+
+foreign import search :: forall              e.
+                               RoomPosition
+                      -> Array PathFinderTarget
+                      ->     (PathFinderOpts e)
+                      -> Eff (path :: PATH | e)  PathFinderResult
+
+foreign import newCostMatrix :: forall              e.
+                                Eff (path :: PATH | e) CostMatrix
+
+foreign import infinity :: Number
+
+defaultPathFinderOpts = PathFinderOpts {
+      roomCallback:    allDefaultCosts
+    , plainCost:       1
+    , swampCost:       5
+    , flee:            false
+    , maxOps:          2000
+    , maxRooms:        16
+    , maxCost:         infinity
+    , heuristicWeight: 1.2
+    }
+
+type RoomCallback e = String -- ^ Room name
+                   -> Eff (path :: PATH | e)
+                           CostMatrix
+
+allDefaultCosts          :: forall e. RoomCallback e
+allDefaultCosts _roomName = newCostMatrix
+
+newtype PathFinderOpts e = PathFinderOpts {
+    roomCallback    :: RoomCallback e
+  , plainCost       :: TileCost
+  , swampCost       :: TileCost
+  , flee            :: Boolean
+  , maxOps          :: Int
+  , maxRooms        :: Int
+  , maxCost         :: Number
+  , heuristicWeight :: Number
+  }
+
+-- * Cost matrix
+set :: forall              e.
+       CostMatrix
+    -> Int
+    -> Int
+    -> TileCost
+    -> Eff (path :: PATH | e) Unit
+set  = runThisEffFn3 "set"
+
+get :: forall              e.
+       CostMatrix
+    -> Int
+    -> Int
+    -> Eff (path :: PATH | e) TileCost
+get  = runThisEffFn2 "get"
+
+clone :: forall              e.
+                                CostMatrix
+      -> Eff (path :: PATH | e) CostMatrix
+clone  = runThisEffFn0 "clone"
+
+newtype SerializedCostMatrix = SerializedCostMatrix Json
+
+derive newtype instance showSerializedCostMatrix :: Show SerializedCostMatrix
+
+serialize :: forall              e.
+             CostMatrix
+          -> Eff (path :: PATH | e) SerializedCostMatrix
+serialize  = runThisEffFn0 "serialize"
+
+foreign import deserialize :: forall              e.
+                              SerializedCostMatrix
+                           -> Eff (path :: PATH | e) CostMatrix
+
---
+++ src/Screeps/Path/Cache.purs
@@ -0,0 +1,30 @@
+-- | This module facilitates caching of the PathFinder's `RoomCallback`.
+module Screeps.Path.Cache where --(Cache, cached, newCache) where
+
+import Prelude
+
+import Control.Monad.Eff
+import Control.Monad.Eff.Ref
+import Data.Maybe
+import Data.StrMap as StrMap
+
+import Screeps.Path as PF
+
+newtype Cache = Cache (Ref (StrMap.StrMap PF.CostMatrix))
+
+cached :: forall a. Cache
+       -> PF.RoomCallback (ref :: REF | a) -- (String -> Eff ( ref :: REF | a ) CostMatrix )
+       -> PF.RoomCallback (ref :: REF | a) -- String -> Eff ( ref :: REF | a ) CostMatrix
+cached (Cache cache) act roomName = do
+    r <- StrMap.lookup roomName <$> readRef cache
+    case r of
+         Nothing -> do
+           v <- act roomName
+           modifyRef cache $ StrMap.insert roomName v
+           pure v
+         Just   v ->
+           pure v
+
+newCache :: forall e. Eff ( ref :: REF | e ) Cache
+newCache =  Cache <$> newRef StrMap.empty
+
