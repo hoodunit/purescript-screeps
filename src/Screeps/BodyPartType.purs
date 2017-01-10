db3e95a3c50c14d8dc84545fdeb474827a8cb06d
Michal Gajda <michal@jewelpaymentech.com>
2017-01-10 10:11:24 +0800

    Body cost summing

--- src/Screeps/BodyPartType.purs
+++ src/Screeps/BodyPartType.purs
@@ -1,7 +1,11 @@
 module Screeps.BodyPartType where
 
-import Data.Generic
+import Control.Category
 import Data.Eq
+import Data.Foldable (sum)
+import Data.Functor
+import Data.Generic
+import Data.Ring
 import Data.Show
 
 newtype BodyPartType = BodyPartType String
@@ -19,12 +23,30 @@ foreign import part_heal :: BodyPartType
 foreign import part_claim :: BodyPartType
 
 foreign import bodypart_cost ::
-  { move :: Int
-  , work :: Int
-  , attack :: Int
-  , carry :: Int
-  , heal :: Int
+  { move          :: Int
+  , work          :: Int
+  , attack        :: Int
+  , carry         :: Int
+  , heal          :: Int
   , ranged_attack :: Int
-  , tough :: Int
-  , claim :: Int }
+  , tough         :: Int
+  , claim         :: Int }
+
+type Body = Array BodyPartType
+
+type Cost = Int
+
+bodyCost :: Body -> Cost
+bodyCost  = sum <<< map bodyPartCost
+
+bodyPartCost                            :: BodyPartType -> Cost
+bodyPartCost p | p == part_claim         = bodypart_cost.claim
+               | p == part_move          = bodypart_cost.move
+               | p == part_attack        = bodypart_cost.attack
+               | p == part_ranged_attack = bodypart_cost.ranged_attack
+               | p == part_tough         = bodypart_cost.tough
+               | p == part_carry         = bodypart_cost.carry
+               | p == part_work          = bodypart_cost.work
+               | p == part_heal          = bodypart_cost.heal
+bodyPartCost _                           = -9999999
 
