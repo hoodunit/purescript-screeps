232ca0e1cb88f693f300e14bf0426afe3777fe4a
Michal Gajda <michal@jewelpaymentech.com>
2017-01-13 03:28:55 +0800

    Encode/Decode for a room object.

--- src/Screeps/RoomObject.purs
+++ src/Screeps/RoomObject.purs
@@ -2,9 +2,20 @@
 module Screeps.RoomObject where
 
 import Control.Category
+import Control.Monad
+import Control.Monad.Eff
+import Control.Monad.Eff.Exception
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Data.Argonaut.Decode.Class
+import Data.Argonaut.Encode.Class
+import Data.Either
+import Data.Function (($))
+import Data.Maybe
+import Data.Monoid
 import Data.Show
+import Data.StrMap as StrMap
 
-import Screeps.FFI (unsafeField)
+import Screeps.FFI (unsafeField, toMaybe, NullOrUndefined)
 import Screeps.Names
 import Screeps.RoomPosition.Type (RoomPosition)
 
@@ -18,6 +29,20 @@ class RoomObject     a
 
 name :: Room -> RoomName
 name = unsafeField "name"
+
+instance encodeJson :: EncodeJson Room where
+  encodeJson = encodeJson <<< name
+
+foreign import lookupRoom :: forall e. RoomName -> Eff e (NullOrUndefined Room)
+
+instance decodeJson :: DecodeJson Room where
+  decodeJson           json = do
+    name <- decodeJson json
+    case unsafePerformEff $ try $ map toMaybe $ lookupRoom name of
+         Left   err      -> Left  $ "Cannot access the room: " <> show name
+                                 <> " because of: "            <> show err
+         Right (Nothing) -> Left  $ "Cannot access room: "     <> show name
+         Right (Just r ) -> Right   r
 
 foreign import data AnyRoomObject :: *
 
