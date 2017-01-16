088651e4684c4019d09a98e8401a502c66fab0ad
Michal Gajda <michal@jewelpaymentech.com>
2017-01-16 22:27:52 +0800

    Add Ord instance for RoomName and CreepName.

--- src/Screeps/Names.purs
+++ src/Screeps/Names.purs
@@ -5,6 +5,7 @@ import Data.Argonaut.Encode.Class
 import Data.Argonaut.Decode.Class
 import Data.Eq (class Eq)
 import Data.Functor(map)
+import Data.Ord (class Ord)
 import Data.Show (class Show)
 
 -- | RoomName allows to enumerate rooms even if they are not active.
@@ -14,6 +15,7 @@ asRoomName :: String -> RoomName
 asRoomName = RoomName
 
 derive newtype instance eqRoomName   :: Eq   RoomName
+derive newtype instance ordRoomName  :: Ord   RoomName
 instance                showRoomName :: Show RoomName where show (RoomName rn) = rn
 instance              encodeRoomName :: EncodeJson RoomName where encodeJson      (RoomName rn) = encodeJson rn
 instance              decodeRoomName :: DecodeJson RoomName where decodeJson = map RoomName <<< decodeJson
@@ -25,6 +27,7 @@ asCreepName :: String -> CreepName
 asCreepName = CreepName
 
 derive newtype instance eqCreepName   :: Eq   CreepName
+derive newtype instance ordCreepName  :: Ord  CreepName
 instance                showCreepName :: Show CreepName where show (CreepName rn) = rn
 instance              encodeCreepName :: EncodeJson CreepName where encodeJson      (CreepName rn) = encodeJson rn
 instance              decodeCreepName :: DecodeJson CreepName where decodeJson = map CreepName <<< decodeJson
