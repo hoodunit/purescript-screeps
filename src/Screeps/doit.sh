85fd9e2d7af1845ed4997ca926eeffddf8cb4129
Matt Walker <matt.g.d.walker@gmail.com>
2022-01-09 17:20:27 -0500

    --amend

--- src/Screeps/doit.sh
+++ src/Screeps/doit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ls | xargs sed -i -e 's/import Screeps.Eff (.*)//'
-ls | xargs sed -i -e 's/Effect/Effect/'
+ls | xargs sed -i -e 's/Eff/Effect/'
 ls | xargs sed -i -e 's/(Eff)/(Effect)/'
-ls | xargs sed -i -e 's/Effect \(.*\)/Effect/'
+ls | xargs sed -i -e 's/Effect \((.*)\)/Effect/'
