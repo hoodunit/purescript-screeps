0e6ac57a8486ddcecf08c04347a0541f2087e574
Matt Walker <matt.g.d.walker@gmail.com>
2022-01-09 19:00:18 -0500

    Remove doit.sh

--- src/Screeps/doit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ls | xargs sed -i -e 's/import Screeps.Eff (.*)//'
-ls | xargs sed -i -e 's/Eff/Effect/'
-ls | xargs sed -i -e 's/(Eff)/(Effect)/'
-ls | xargs sed -i -e 's/Effect \((.*)\)/Effect/'
-ls | xargs sed -i -e 's/Effectect/Effect/'
-ls | xargs sed -i -e 's/import Control.Monad.Effect/import Effect/'
